b92da3aa3e722117a7095736de463f8ba695e9be
Undangaann <asktika54@gmail.com>
2026-09-17 10:19:57 +0800

    Update config.js

--- js/config.js
+++ js/config.js
@@ -69,8 +69,7 @@ const WEDDING_CONFIG = {
         "assets/images/gallery-1.jpg",
         "assets/images/gallery-2.jpg",
         "assets/images/gallery-3.jpg",
-        "assets/images/gallery-4.jpg",
-        "assets/images/cover-1.jpg"
+        "assets/images/gallery-4.jpg"
     ],
 
     music: "assets/music/music.mp3",
